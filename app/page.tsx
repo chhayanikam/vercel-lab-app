8f756ce594ac7517249241105c98e100a02d0c7e
admin-agentic <chhaya.nikam@gmail.com>
2026-05-01 10:16:03 +0530

    updated the page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🚀 Deployed on Vercel!</h1>
+      <h1>🔥 Updated Version!</h1>
       <p>This is my first Vercel deployment.</p>
     </div>
  
